7da617c3f7c3ceeda29a51c32b3ca02b865a7ddf
jhaeseop <jhaeseop@gmail.com>
2025-01-22 13:25:30 +0900

    sensorweb first commit

---
+++ picam/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+import paho.mqtt.client as mqtt
+import base64
+import cv2
+import numpy as np
+import threading
+
+
+# Create your views here.
+
+# MQTT 브로커 정보
+MQTT_BROKER = "192.168.0.44"
+MQTT_PORT = 1883
+MQTT_TOPIC = "raspberrypi/camera"
+
+# 글로벌 변수로 프레임 저장
+current_frame_base64 = None
+
+def on_message(client, userdata, msg):
+    global current_frame_base64
+    # MQTT 메시지를 base64로 저장
+    current_frame_base64 = msg.payload.decode('utf-8')
+
+def mqtt_subscribe():
+    mqtt_client = mqtt.Client()
+    mqtt_client.on_message = on_message
+    mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
+    mqtt_client.subscribe(MQTT_TOPIC)
+    mqtt_client.loop_forever()
+
+# MQTT Subscribe를 백그라운드 스레드에서 실행
+mqtt_thread = threading.Thread(target=mqtt_subscribe, daemon=True)
+mqtt_thread.start()
+
+def picam(request):
+    global current_frame_base64
+    return render(request, 'picam/picam.html', {'current_frame_base64': current_frame_base64})
---
+++ sensorweb/views.py
@@ -0,0 +1,47 @@
+import json
+import random
+import time
+import requests
+from django.http import HttpResponse, JsonResponse, StreamingHttpResponse
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.views.generic import View
+from django.views.generic import TemplateView
+from chartjs.views.lines import BaseLineChartView
+from paho.mqtt import client as mqtt_client
+from .models import sensors
+
+
+# Create your views here.
+
+def index(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    print(f'x_forwarded_for = {x_forwarded_for}')
+    if x_forwarded_for:
+        request_ip = x_forwarded_for.split(',')
+        print(f'ip:HTTP_X_FORWARDED_FOR = {request_ip}')
+    else:
+        request_ip = request.META.get('REMOTE_ADDR')
+        print(f'ip:REMOTE_ADDR = {request_ip}')
+    return render(request, 'sensorweb/main.html')
+
+def sensor(request):
+    # AJAX 요청 처리 또는 템플릿 렌더링
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        data = sensors.objects.order_by('-time')[:60]
+        return JsonResponse({'data': list(data.values())})
+    else:
+        return render(request, 'sensorweb/table_chart.html')  # 템플릿 렌더링
+
+
+
+def pages_view(request, num_page):
+    return render(request, f'samples/sample{num_page}.html')
+
+def sub_view(request, n_page, n_sect):
+    return render(request, f'sub/{n_page}_{n_sect}.html')
+
+def file_upload(request):
+    return render(request, 'fileupload/fileupload.html')
+
